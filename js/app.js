0fe5fcfd242a5bd904e873a931bd6da06c352b7a
Ian Walter <iankwalter@me.com>
2013-10-07 08:26:49 -0400

    Working

--- js/app.js
+++ js/app.js
@@ -8,28 +8,30 @@
 var app = angular.module('ab', ['services.breadcrumbs'])
   .config(['$routeProvider', function($routeProvider) {
       $routeProvider
-          .when('/', { controller: 'HomeController', templateUrl: '/vw/home.html', label: 'Customers' })
+          .when('/', { controller: 'HomeController', templateUrl: '/vw/home.html', label: 'Home' })
           .when('/stock/:stock', { controller: 'StockController', templateUrl: '/vw/stock.html', label: 'Stock' })
           .when('/stock/:stock/detail', { controller: 'StockDetailController', templateUrl: '/vw/stock-detail.html', label: 'Stock Detail' })
           .otherwise({ redirectTo: '/' });
   }]);
 
+app.stocks = {
+  'AAPL': { symbol: 'AAPL', price: '493.03', revenue: '3,303,403,203' },
+  'TSLA': { symbol: 'TSLA', price: '182.45', revenue: '121,203,542' }
+};
+
 app.controller('HomeController', ['$scope', 'breadcrumbs', function($scope, breadcrumbs) {
   $scope.breadcrumbs = breadcrumbs;
 
   $scope.summary = "This is the Home page";
-  $scope.stocks = {
-    'AAPL': { symbol: 'AAPL', price: '493.03', revenue: '3,303,403,203' },
-    'TSLA': { symbol: 'TSLA', price: '182.45', revenue: '121,203,542' }
-  };
+  $scope.stocks = app.stocks;
 }]);
 
 app.controller('StockController', ['$scope', '$routeParams', function($scope, $routeParams) {
-  $scope.stock = $scope.stocks[$routeParams['stock']];
+  $scope.stock = app.stocks[$routeParams['stock']];
   $scope.summary = "This is the Stock page";
 }]);
 
-app.controller('StockDetailController', ['$scope', function($scope) {
-  $scope.stock = $scope.stocks[$routeParams['stock']];
+app.controller('StockDetailController', ['$scope', '$routeParams', function($scope, $routeParams) {
+  $scope.stock = app.stocks[$routeParams['stock']];
   $scope.summary = "This is the Stock Detaill page";
 }]);
